a99b65912553e65fc0d12b1723494233d0aa5a04
RuslanMiki <mikitchenko_ruslan@ukr.net>
2023-05-04 13:21:52 +0200

    finish

--- src/app.js
+++ src/app.js
@@ -14,8 +14,11 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static("public"));
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', authRouter)
+
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
